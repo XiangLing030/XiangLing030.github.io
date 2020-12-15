555f8f4615fa8c32578c6f59dae2a2d79907dc42
Xiang Ling <xling030@gmail.com>
2020-12-15 20:29:02 +0800

    2028

---
+++ week06/scripts/fetch.js
@@ -0,0 +1,9 @@
+function GetBookings() {
+    let url = 'https://api.sheety.co/b064d12af8aa83176c9e735b8ead7e47/bookingApp/bookings';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            console.log(json.bookings);
+        });
+}
