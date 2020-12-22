fc7122af4f1d11e560845390ae40dfedbc7cc62c
Xiang Ling <xling030@gmail.com>
2020-12-22 20:29:49 +0800

    2029

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -8,15 +8,13 @@ function GetBookings() {
             let bookingList = document.getElementById("booking-list");
             let bookingIds = [];
 
-            let jsonDiv = document.getElementById("json");
-            jsonDiv.innerHTML = "";
-            jsonDiv.innerHTML = json.bookings;
-
             for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
                 let gPax = json.bookings[i].pax;
                 let gId = json.bookings[i].id;
+                let gRemarks = json.bookings[i].remarks;
+                let buttonId = "delete" + gId;
 
                 bookingList.innerHTML += gId + "-" + gName + ", " + gEmail + ", pax: " + gPax + "<br>";
             }
